91b2ce2a0b32f23c9b26816ee3d117716b10b670
Nick Hatch <nicholas.hatch@gmail.com>
2016-03-22 19:59:35 -0700

    comments

--- smartnet_parser.cc
+++ smartnet_parser.cc
@@ -9,7 +9,7 @@ double SmartnetParser::getfreq(int cmd) {
 	float freq;
 	if (cmd < 0x1b8) {
 		freq = float(cmd * 0.025 + 851.0125);
-	} else if (cmd < 0x230) {
+	} else if (cmd < 0x230) { //rebanded splinter chnnels
 		freq = float(cmd * 0.025 + 851.0125 - 10.9875);
 	} else {
 		freq = 0;
@@ -31,6 +31,8 @@ std::vector<TrunkMessage> SmartnetParser::parse_message(std::string s) {
 	std::vector<std::string> x;
 	boost::split(x, s, boost::is_any_of(","), boost::token_compress_on);
 
+	// Smartnet talkgroups use low 16 bits for status bits, mod16 for actual talkgroup
+	// http://wiki.radioreference.com/index.php/Motorola_Type_II_Smartnet
 	long address = atoi( x[0].c_str() ) & 0xFFF0;
 	//int groupflag = atoi( x[1].c_str() );
 	int command = atoi( x[2].c_str() );
@@ -38,12 +40,12 @@ std::vector<TrunkMessage> SmartnetParser::parse_message(std::string s) {
 	x.clear();
 	vector<string>().swap(x);
 
-	if (command < 0x2d0) {
+	if (command < 0x2d0) { //720
 		if (  (address != 56016) && (address != 8176)) {  // remove this later to make it more general
 			message.talkgroup = address;
 			message.freq = getfreq(command);
 			if ( lastcmd == 0x308) {
-				// Channel Grant
+				// Channel Grant - 776
 				message.message_type = GRANT;
 			} else {
 				// Call continuation
